fb805ca90785c180260451f7125ee826fbdf5b11
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2023-01-10 15:36:16 +0900

    １月２週間目の進捗

---
+++ Incubators/UtilsIncubator/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace UtilsIncubator;
+
+
+public static class StringExtensions
+{
+
+  public static bool IsNonEmpty(this string self)
+  {
+    return !String.IsNullOrEmpty(self);
+  }
+  
+}
--- Infrastructure/Elements/Client/Components/SharedReusable/Controls/CompoundControlShell/CompoundControlShell.razor.cs
+++ Infrastructure/Elements/Client/Components/SharedReusable/Controls/CompoundControlShell/CompoundControlShell.razor.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
-
+using Utils;
 
 namespace Client.Components.SharedReusable.Controls.CompoundControlShell;
 
@@ -10,4 +10,100 @@ public partial class CompoundControlShell : ComponentBase
   [Parameter]
   public string SpaceSeparatedAdditionalCSS_Classes { get; set; }
   
+  [Parameter]
+  public RenderFragment ChildContent { get; set; }
+ 
+  [Parameter]
+  public string? Label { get; set; }
+  
+  [Parameter]
+  public string? Guidance { get; set; }
+
+
+  [Parameter] 
+  public bool Required { get; set; }
+  
+  [Parameter] 
+  public bool DisplayAppropriateBadgeIfInputIsRequired { get; set; }
+  
+  [Parameter] 
+  public bool DisplayAppropriateBadgeIfInputIsOptional { get; set; }
+  
+  
+  [Parameter]
+  public string? CoreElementHTML_ID { get; set; }
+  
+  [Parameter]
+  public string? LabelElementHTML_ID { get; set; }
+  
+  
+  [Parameter]
+  public string MainSlotSpaceSeparatedAdditionalCSS_Classes { get; set; }
+  
+  
+  [Parameter]
+  public bool MustActivateAppropriateHighlightIfAnyErrorsMessages { get; set; }
+
+  [Parameter]
+  public IEnumerable<string> errorsMessages { get; set; } = Array.Empty<string>();
+  
+  
+  public enum StandardThemes
+  {
+    regular
+  }
+  
+  [Parameter]
+  public string Theme { get; set; }
+
+  [Parameter]
+  public bool AreThemesExternal { get; set; }
+  
+  
+  public enum StandardGeometricVariations
+  {
+    regular
+  }
+  
+  [Parameter]
+  public string Geometry { get; set; }
+  
+  
+  public enum StandardDecorativeVariations
+  {
+    notice,
+    error,
+    warning,
+    success,
+    guidance,
+    question
+  }
+  
+  [Parameter]
+  public string Decoration { get; set; }
+
+  
+  private string rootElementModifierCSS_Classes
+  {
+    get
+    {
+      
+      return new List<string>().
+          AddElementToEndIf(
+            $"AttentionBox--YDF__${ Theme.ToLowerCamel() }Theme",
+            _ => Enum.GetNames(typeof(StandardThemes)).Length > 1 && !AreThemesExternal
+          ).
+          AddElementToEndIf(
+            $"AttentionBox--YDF__${ Geometry.ToLowerCamel() }Geometry",
+            _ => Enum.GetNames(typeof(StandardGeometricVariations)).Length > 1
+          ).
+          AddElementToEndIf(
+            $"AttentionBox--YDF__${ Decoration.ToLowerCamel() }Decoration",
+            _ => Enum.GetNames(typeof(StandardDecorativeVariations)).Length > 1
+          ).
+          StringifyEachElementAndJoin("");
+
+    }
+  }
+
 }
