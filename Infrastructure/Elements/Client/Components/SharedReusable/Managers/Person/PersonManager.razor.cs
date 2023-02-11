0f2adee7bc2a63acec0764419be41a9e47df3a09
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2023-02-11 14:37:12 +0900

    2月の進捗

--- Infrastructure/Elements/Client/Components/SharedReusable/Managers/Person/PersonManager.razor.cs
+++ Infrastructure/Elements/Client/Components/SharedReusable/Managers/Person/PersonManager.razor.cs
@@ -8,6 +8,22 @@ public partial class PersonManager : ComponentBase
 {
 
   [Parameter]
-  public string SpaceSeparatedAdditionalCSS_Classes { get; set; }
+  public string spaceSeparatedAdditionalCSS_Classes { get; set; }
+
+  
+
+
+  /* === ルーチン ====================================================================================================== */
+  /* --- ID生成 ------------------------------------------------------------------------------------------------------ */
+  private readonly string COMPONENT_ID = PersonManager.generateComponentID();
+  private string HEADING_ID => $"{ this.COMPONENT_ID }-HEADING";
+  
+  private static uint counterForComponentID_Generating = 0;
+
+  private static string generateComponentID()
+  {
+    PersonManager.counterForComponentID_Generating++;
+    return $"PERSON_MANAGER-{ PersonManager.counterForComponentID_Generating }";
+  }
   
 }
