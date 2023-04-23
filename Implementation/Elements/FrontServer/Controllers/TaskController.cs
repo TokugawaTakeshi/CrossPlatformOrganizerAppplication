3a729b20e663c96d1d91ac2a8d6e15fc231a4f90
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2023-04-23 12:54:50 +0900

    TaskControllerを追加し、課題取得試験に成功

---
+++ Implementation/Elements/FrontServer/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using Task = CommonSolution.Entities.Task;
+using CommonSolution.Gateways;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontServer.Controllers;
+
+
+[Route("api/tasks")]
+[ApiController]
+public class TaskController : ControllerBase
+{
+
+  private readonly ITaskGateway taskGateway = FrontServerDependencies.Injector.gateways().Task;
+
+  [HttpGet]
+  public async System.Threading.Tasks.Task<ActionResult<Task[]>> retrieveAllTasks()
+  {
+    
+    return Ok(await this.taskGateway.RetrieveAll());
+  } 
+
+}
