c793c88af8f1261a7dfb5b1e8ee9693405e475ac
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2023-08-15 19:12:13 +0900

    PersonEmailInputtedDataValidationクラスを追加

---
+++ Implementation/Elements/Client/Data/FromUser/Entities/Person/PersonAvatarURI_InputtedDataValidation.cs
@@ -0,0 +1,17 @@
+﻿using FrontEndFramework.InputtedValueValidation;
+
+
+namespace Client.Data.FromUser.Entities.Person;
+
+
+internal class PersonEmailInputtedDataValidation : InputtedValueValidation
+{
+  public PersonEmailInputtedDataValidation(
+    bool? isInputRequired = CommonSolution.Entities.Person.EmailAddress.IS_REQUIRED, 
+    string? requiredInputIsMissingValidationErrorMessage = "メールアドレスは必須となります。お手数ですがメールアドレスを入力して下さい。"
+  ): base(
+    hasValueBeenOmitted: rawValue => String.IsNullOrEmpty(rawValue as string),
+    isInputRequired,
+    requiredInputIsMissingValidationErrorMessage: requiredInputIsMissingValidationErrorMessage
+  ) {}
+}
